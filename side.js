54f30cedf951f88d6f90209ea5cb138737a57b93
Victor Sosa <vsosa01127@gmail.com>
2022-05-20 22:29:55 -0400

    updates

--- side.js
+++ side.js
@@ -4,12 +4,17 @@ var projectDescription = document.getElementById("descrip");
 descriptionHeader.style.visibility='hidden';
 projectDescription.style.visibility='hidden';
 
-function Show(description, url=""){
+function Show(name, description, url){
     
     descriptionHeader.style.visibility='visible';
     projectDescription.style.visibility='visible';
-
+    descriptionHeader.innerHTML=name;
     projectDescription.innerHTML= description;
+
+    var projectLink = document.getElementById("link-project-description");
+    projectLink.hidden=false;
+    projectLink.href = url;
+
 
 
 }
